ce2b9f7934d1193adec7574a756b56f60539fc33
DXXN <pexwp@mail.ru>
2024-02-08 23:23:41 +0300

    database interface has been fully completed

--- Server/dbhandler.cpp
+++ Server/dbhandler.cpp
@@ -1,7 +1,48 @@
 #include "dbhandler.h"
+#include "qthread.h"
+#include <QScopedPointer>
 
 DBhandler::DBhandler(QObject *parent)
     : QObject{parent}
 {
 
 }
+
+DBhandler::~DBhandler(){
+    qDebug() << "db handler destroyed";
+}
+
+void DBhandler::open()
+{
+    db = QSqlDatabase::addDatabase(dbDriveName);
+    db.setDatabaseName(dbName);
+    db.setHostName(hostName);
+    db.open();
+}
+
+void DBhandler::write(const QString imageName, const QString path, const QString date)
+{
+    QSqlQuery query;
+    query.prepare("INSERT INTO images(name, path, date) VALUES(?, ?, ?)");
+    query.addBindValue(imageName);
+    query.addBindValue(path);
+    query.addBindValue(date);
+
+    if(query.exec()){
+        qDebug() << "writed";
+    }
+    else{
+        qDebug() << query.lastError();
+    }
+}
+
+void DBhandler::read()
+{
+    QSqlQuery query;
+    QSqlQueryModel *model = new QSqlQueryModel;
+    query.prepare("SELECT * FROM images");
+    query.exec();
+    model->setQuery(std::move(query));
+    emit readingFinished(model);
+}
+
--- Server/dbhandler.h
+++ Server/dbhandler.h
@@ -1,14 +1,37 @@
 #pragma once
 
 #include <QObject>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlError>
+#include <QSqlQueryModel>
 
 class DBhandler : public QObject
 {
     Q_OBJECT
 public:
     explicit DBhandler(QObject *parent = nullptr);
+    ~DBhandler();
+    void logIn(const QString login,const QString password);
+    void open();
+    void close();
+
+public slots:
+    void write(const QString imageName,const QString path,const QString date);
+    void read();
 
 signals:
+    void readingFinished(QSqlQueryModel* model);
 
+private:
+    QSqlDatabase db;
+    quint16 port;
+    QString dbName="C:/Work/repos/testForCB/testDb.db";
+    QString hostName="localhost";
+    QString userName;
+    QString password;
+    QString dbDriveName="QSQLITE";
+    QString dbPath;
 };
 
