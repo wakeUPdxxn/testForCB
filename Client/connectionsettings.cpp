5190d8ce79c73d6d7d36b0a22df4d3cfa7b7facc
DXXN <pexwp@mail.ru>
2024-02-10 23:42:35 +0300

    New window for managing connection data;

---
+++ Client/connectionsettings.cpp
@@ -0,0 +1,50 @@
+#include "connectionsettings.h"
+
+ConnectionSettings::ConnectionSettings(QWidget *parent)
+    : QWidget{parent}
+{
+    setAttribute(Qt::WA_DeleteOnClose);
+    setWindowTitle("Connection Settings");
+    this->setStyleSheet("QWidget { background-color: #2F4F4F;}"
+                        "QLabel { color : black; }"
+
+                        "QPushButton { "
+                        "background-color: blue; "
+                        "border: 2px solid;"
+                        "border-radius: 8px }"
+
+                        "QLineEdit { "
+                        "border: 2px solid gray;"
+                        "border-radius: 8px };"
+                        );
+
+    address_lbl.setText("Server address: ");
+    address_lbl.setAlignment(Qt::AlignHCenter| Qt::AlignRight);
+
+    port_lbl.setText("Server port: ");
+    address_lbl.setAlignment(Qt::AlignHCenter | Qt::AlignRight);
+
+    save.setText("Save");
+
+    isAutoConnect.setText("Auto connect");
+
+    layout.addWidget(&address_lbl, 0, 0);
+    layout.addWidget(&addrInput, 0, 1);
+    layout.addWidget(&port_lbl, 1, 0);
+    layout.addWidget(&portInput, 1, 1);
+    layout.addWidget(&save, 2, 0);
+    layout.addWidget(&isAutoConnect, 2, 1);
+
+    setLayout(&layout);
+
+    connect(&save,&QPushButton::clicked,this,&ConnectionSettings::onSavePressed);
+    this->show();
+}
+
+void ConnectionSettings::onSavePressed()
+{
+    //обращаемся к классу управления данными для записи в json файл
+}
+
+
+
---
+++ Client/connectionsettings.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <QWidget>
+#include <QLabel>
+#include <QGridLayout>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QCheckBox>
+
+class ConnectionSettings : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ConnectionSettings(QWidget *parent = nullptr);
+
+private:
+    QLabel address_lbl;
+    QLabel port_lbl;
+    QLineEdit addrInput;
+    QLineEdit portInput;
+    QPushButton save;
+    QString address;
+    QString port;
+    QGridLayout layout;
+    QCheckBox isAutoConnect;
+
+private slots:
+    void onSavePressed();
+};
+
